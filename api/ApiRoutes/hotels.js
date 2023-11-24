7fb46e6bfef6fe91af40aaad223ea6ce9ac395ef
frederic <paulpoiu123@gmail.com>
2023-11-24 22:41:46 +0100

    updated

--- api/ApiRoutes/hotels.js
+++ api/ApiRoutes/hotels.js
@@ -1,55 +1,14 @@
 import express from "express"
-import Hotel from "../models/Hotel.js"
-import { errorMessage } from "./errorMessage.js"
-
+import { createHotel, deleteHotel, getHotel, updatedHotel } from "../RoutesController/hotel.js" 
 
 const router = express.Router()
-//get 就像app.get 運用router把app.get可以分門別類 
-// router.get("/",(req,res)=>{
-//     res.send("這邊是hotelsApi End points連接點")
-//     console.log("這邊是hotelsApi End points連接點")
-// })
-router.post("/",async(req,res,next)=>{ //新增next
-    const newHotel = new Hotel(req.body) 
-    try {
-        const saveHotel = await newHotel.save()
-        res.status(200).json(saveHotel)
-    } catch (error) {
-        next(errorMessage(500,"資料上傳錯誤請確認格式",error)) //後來我們想要客製化的
-    }
-})
+//創建第一筆資料
+router.post("/",createHotel)
+//抓取第一筆資料練習
+router.get("/find/:id",getHotel)
+//將第一筆資料做修改練習
+router.put("/:id",updatedHotel)
+//刪除資料
+router.delete("/:id",deleteHotel)
 
-router.get("/find/:id",async(req,res)=>{
-    const id = req.params.id;
-    try {
-        const hotel = await Hotel.findById(id)
-        res.status(200).json(hotel)
-    } catch (error) {
-        next(errorMessage(500,"資料上傳錯誤請確認格式",error)) //後來我們想要客製化的
-    }
-})
-
-router.put("/:id",async(req,res)=>{
-    const id = req.params.id;
-    const body = req.body;
-    try {
-        const updatedHotel = await Hotel.findByIdAndUpdate(id,{$set:body},{new:true})
-        res.status(200).json(updatedHotel)
-    } catch (error) {
-        next(errorMessage(500,"資料上傳錯誤請確認格式",error)) //後來我們想要客製化的
-    } 
- })
-
- router.delete("/:id",async(req,res) => {
-    const id = req.params.id;
-    try {
-        res.status(200).json("刪除成功")
-    } catch (error) {
-        next(errorMessage(500,"資料上傳錯誤請確認格式",error)) //後來我們想要客製化的
-    }
-    
- })
-
-
-
-export default router;
+export default router
