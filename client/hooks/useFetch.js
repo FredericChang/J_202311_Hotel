d9165decf043dacc1db830497fd8e8c03bba54a2
frederic <paulpoiu123@gmail.com>
2023-11-26 17:22:25 +0100

    Add proxy in configurator json

---
+++ client/hooks/useFetch.js
@@ -0,0 +1,28 @@
+import react, {useEffect} from "react";
+import axios from "axios"
+
+const useFetch = (url) => {
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState("")
+
+    useEffect(() => {
+        const fetchData = async()=>{
+            setLoading(true); //來確定連線時成功的開始
+            try{
+                const response = await axios.get(url);
+                //axios.get最重要的資料 且url是feature那邊傳入的/hotels
+                setData(resonse.data);
+            }catch(error){
+                setError(error)//如果有錯誤也紀錄進去setError State
+            }
+            setLoading(false);//代表連線時成功的結束
+        }
+        fetchData()
+    }, []);
+    return {
+        data,
+        loading,
+        error
+    }
+}
