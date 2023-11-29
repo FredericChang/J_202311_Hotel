e6db9dc8fef050044605aa4517dc49133b13197b
frederic <paulpoiu123@gmail.com>
2023-11-30 00:23:25 +0100

    Added files.

--- client/src/context/LoginContext.js
+++ client/src/context/LoginContext.js
@@ -1,5 +1,13 @@
 
 
+import { createContext, useEffect, useReducer } from "react"
+import { login_failure, login_success, logout, start_login } from "../constants/actionTypes";
+const INITIAL_STATE = {
+    user: JSON.parse(localStorage.getItem("user")) || null,
+    loading: false,
+    error: null
+}
+export const LoginContext = createContext(INITIAL_STATE);
 const LoginReducer = (state, action) => {
     switch (action.type) {
         case start_login:
@@ -29,4 +37,22 @@ const LoginReducer = (state, action) => {
         default:
             return state
     }
+}
+export const LoginContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(LoginReducer, INITIAL_STATE)
+    useEffect(() => {
+            localStorage.setItem("user", JSON.stringify(state.user))
+        }, [state.user]
+    )
+    return (
+        <LoginContext.Provider
+            value={{
+                user: state.user,
+                loading: state.loading,
+                error: state.error,
+                dispatch,
+            }}>
+            {children}
+        </LoginContext.Provider>
+    )
 }
